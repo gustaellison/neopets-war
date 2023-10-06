638be5f142f1f0d7a9b63bed2354da845d7689d9
Augusta Ellison <gustaellison@gmail.com>
2023-10-06 12:34:21 -0400

    pseudocode

---
+++ js/app.js
@@ -0,0 +1,56 @@
+/*----- constants -----*/
+
+/*----- app's state (variables) -----*/
+
+/*----- event listeners -----*/
+
+/*----- functions -----*/
+
+
+
+// Pseudocode:
+// 1. Assign random values to fighters and enemies
+    //  1.1 Assign a random value between 1 and 10 to the players fighters
+            // Create variables for each fighter - Select the FIGHTERS (f-1 through f-6 div ids)
+            // Create variable for fighter power CREATE JS OBJECT/CLASS
+            // Assign random fighter values  
+        //  1.1.2 Generate the ENEMY fighting power and image
+            // Create variable for Enemy power
+            // Create variable for Enemy image - placeholder fighter image
+            // Set power and image to be random
+    // set starting scores to zero    
+// 
+// On click
+// 2. Populate the fighter and enemy's images and fighting powers to view
+    // create constants for the eggs on the page
+    // create variable for the fighting powers in arena
+    // Update the enemy variables to the random values that have been preselected in the starting state
+    // Replace fighters placeholder egg and fighting score in arena
+// 3. Compare values of fighter and enemy to determine winner
+    // create variable for the text that says who won
+    // create variable for winner
+        // compare fighter power to enemy power
+    // assign player with higher score to winner
+    // set text for who won
+    // variables for enemy and fighter scores
+        // add one point for each win
+
+// Play again
+    // reset starting values
+        // reset the eggs
+        // do we want to shuffle the values of the fighters? or keep the same fighters and values and remove them after theyve fought?
+        // do we want to set it to 3 rounds? and remove each character after theyve been used
+        // generate random enemy value agian
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
