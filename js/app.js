46d066f3395b601e90a806db0ca984bb19da7617
Augusta Ellison <gustaellison@gmail.com>
2023-10-09 16:32:08 -0400

    images

--- js/app.js
+++ js/app.js
@@ -10,7 +10,7 @@ const FIGHTERSARR = [
 
 
 const ENEMIES = [
-    'https://vfxvoice.com/wp-content/uploads/2021/12/PIX-9-Screen-Shot-2021-10-04-at-18.44.31.jpg', 
+    'https://static.displate.com/857x1200/displate/2019-04-25/83634e3f1cc5fbd9458869d3e9dc8a6b_1778fe4c74eb32f8f6d2bf47e613a028.jpg', 
     'https://upload.wikimedia.org/wikipedia/en/b/bd/Joffrey_Baratheon-Jack_Gleeson.jpg',
     'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/1dbc1935-6542-4ee3-822f-135cff4ba62c/df98de6-3bd0eb87-f58b-4726-ad76-ce20a4c8f570.png/v1/fill/w_485,h_605/gollum__1____transparent__by_speedcam_df98de6-fullview.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9NjA1IiwicGF0aCI6IlwvZlwvMWRiYzE5MzUtNjU0Mi00ZWUzLTgyMmYtMTM1Y2ZmNGJhNjJjXC9kZjk4ZGU2LTNiZDBlYjg3LWY1OGItNDcyNi1hZDc2LWNlMjBhNGM4ZjU3MC5wbmciLCJ3aWR0aCI6Ijw9NDg1In1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmltYWdlLm9wZXJhdGlvbnMiXX0.Q25iqft9m-DIaaiS-3gSvVU69efaZnVxfohOxSt5wVM' ]    
 /*----- app's state (variables) -----*/
@@ -26,8 +26,8 @@ const playerArenaEl = document.getElementById('your-fighter')
 const enemyArenaEl = document.getElementById('enemy-fighter')
 
 
-let playerScore = 0;
-let enemyScore = 0;
+let playerScore;
+let enemyScore;
 let fightingPower0;
 let fightingPower1;
 let enemyPower; 
@@ -37,6 +37,9 @@ let f0 = document.getElementById('0')
 // f0 = FIGHTERSARR[0].points
 
 let f1 = document.getElementById('1')
+
+// playAgainEl.addEventListener('click', init())
+
 // f1 = FIGHTERSARR[1].points
 const f0CardText = document.getElementById('f0valuetext')
 f0CardText.textContent = FIGHTERSARR[0].points
@@ -125,29 +128,8 @@ function findWinner1() {
     } else {
         return 'the enemy beat you'
     }
+    
 }
-
-
-
-
-
-//console.log(fighterEls)
-// let fighterPowers = fighterEls[1].power
-
-// let powerArr = fighterEls
-
-
-
-/*----- event listeners -----*/
-/*----- functions -----*/
-
-
-
-//ARRAY for fighters to get objects from
-
-
-    //setting image for each fighter
-
 
 
 
@@ -158,9 +140,13 @@ init()
 function init () {
 
 fighterEls.forEach((obj, idx) =>{
+    // idx = Math.floor(Math.random() * FIGHTERSARR.length)
     obj.src = FIGHTERSARR[idx].imgUrl
     obj.power = FIGHTERSARR[idx].points
     enemyPower= Math.floor(Math.random() * 10 + 1)
+
+    playerScore = 0;
+    enemyScore = 0;
 })
 
 
